5ef85efa068980bb05737ab62262f91b1eb124cd
Michela <micheladellagatta1@gmail.com>
2025-11-12 15:52:23 +0100

    Refactor Actors component to separate male and female actors state management

--- src/components/Actors.jsx
+++ src/components/Actors.jsx
@@ -3,36 +3,33 @@ import axios from "axios";
 import ActorsCard from "./ActorsCard";
 
 export default function Actors({ actorsEndpoint, actressesEndpoint }) {
+  const [maleActors, setMaleActors] = useState([]);
+  const [femaleActors, setFemaleActors] = useState([]);
   const [actors, setActors] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
       .get(actorsEndpoint)
-      .then((res) => {
-        setActors(res.data);
-      })
+      .then((res) => setMaleActors(res.data))
       .catch((err) => console.error(err));
   }, [actorsEndpoint]);
 
   useEffect(() => {
     axios
       .get(actressesEndpoint)
-      .then((res) => {
-        setActors((prev) => [...prev, ...res.data]);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error(err);
-        setLoading(false);
-      });
+      .then((res) => setFemaleActors(res.data))
+      .catch((err) => console.error(err));
   }, [actressesEndpoint]);
 
+  useEffect(() => {
+    setActors([...maleActors, ...femaleActors]);
+    setLoading(false);
+  }, [maleActors, femaleActors]);
+
   return (
-    <>
-      <div className="container my-4">
-        {loading || <ActorsCard actors={actors} />}
-      </div>
-    </>
+    <div className="container my-4">
+      {loading || <ActorsCard actors={actors} />}
+    </div>
   );
 }
